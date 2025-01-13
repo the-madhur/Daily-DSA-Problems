bb43ea4f7f5c34732b806b50bdd8e23041f42246
Madhur Chaturvedi <madhurhp245@gmail.com>
2025-01-13 23:57:46 +0530

    updated file with problem statement

--- Pattern1.java
+++ Pattern1.java
@@ -1,3 +1,41 @@
+/** Problem statement
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* * *
+* * *
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 25
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+* * *
+* * *
+* * *
+Explanation Of Sample Input 1 :
+For N = 3, fill all the rows and columns in 3x3 matrix with ‘*’.
+Sample Input 2 :
+1
+Sample Output 2 :
+*
+Sample Input 3 :
+4
+Sample Output 3 :
+* * * *
+* * * *
+* * * *
+* * * *
+
+**/
+
 import java.util.Scanner;
 public class Pattern1
 {
