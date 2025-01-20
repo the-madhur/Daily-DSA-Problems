b5d068a46e790f13737e209232947cb58e52db22
the-madhur <madhurhp245@gmail.com>
2025-01-21 00:30:36 +0530

    hw patterns left

--- LbAllPatterns.java
+++ LbAllPatterns.java
@@ -198,39 +198,212 @@ public class LbAllPatterns
         // }
         
         // Solid Pyramid   - Full Pyramid + Inverted Full Pyramid
-        System.out.println("Enter value of n5 for Solid Pyramid");
-        int n6 = sc.nextInt();
+        // System.out.println("Enter value of n5 for Solid Pyramid");
+        // int n6 = sc.nextInt();
 
         // Full Pyramid 
-        for(int row = 0;row<n6;row++)
+        // for(int row = 0;row<n6;row++)
+        // {
+        //     // space
+        //     for(int col =0;col<n6-(row + 1);col++)
+        //     {
+        //         System.out.print(" ");
+        //     }
+
+        //     // star 
+        //     for(int col =0;col<row+1;col++)
+        //     {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // // Inverted Full Pyramid 
+        // for(int row = 0;row<n6;row++)
+        // {
+        //     // space
+        //     for(int col =0;col<row;col++)
+        //     {
+        //         System.out.print(" ");
+        //     }
+
+        //     // star 
+        //     for(int col =0;col<n6-row;col++)
+        //     {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // HOLLOW DIAMOND 
+        // System.out.println("Enter value of n7 for Hollow  Diamond");
+        // int n7 = sc.nextInt();
+
+        // // Hollow Diamond - for this we broke in two parts upper part an down part
+
+        // // upper part
+        // for(int row = 0; row<n7; row++)
+        // {
+        //     // space 
+        //     for(int col = 0;col<n7-1-row;col++)
+        //     {
+        //             System.out.print(" ");
+        //     }
+
+        //     // star
+        //     for(int col =0;col<2*row+1;col++)
+        //     {
+        //         // if first character or if last character 
+        //         if(col==0)
+        //         {
+        //             System.out.print("*");
+        //         }
+        //         else if(col==2*row)
+        //         {
+        //             System.out.print("*");
+        //         }
+        //         else
+        //         {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println(); 
+        // }
+
+        // // down part
+        // for(int row = 0;row<n7;row++)
+        // {
+        //     // space 
+        //     for(int col =0;col<row;col++)
+        //     {
+        //             System.out.print(" ");
+        //     }
+
+        //     // star 
+        //     for(int col = 0;col<2*n7-(2*row+1);col++)
+        //     {
+        //         // if first or last character
+        //         if(col ==0 || col == 2*n7-2*row-2)
+        //         {
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // Flipped Solid Diamond
+        
+        // Broke into two parts upper and down 
+        // upper part - broke into 3 parts 
+        // down part - broke into 3 parts 
+
+        // System.out.println("Enter value of n8 for Flippd Solid Diamond");
+        // int n8 = sc.nextInt();
+
+        // // first part 
+        // for(int row =0;row<n8;row++)
+        // {
+        //     // half pyramid
+        //     for(int col =0; col<n8-row;col++)
+        //     {
+        //         System.out.print("*");
+        //     }
+
+        //     // space wala full pyramid 
+        //     for(int col =0; col<2*row+1;col++)
+        //     {
+        //         System.out.print(" ");
+        //     }
+
+        //     // half pyramid 
+        //     for(int col=0;col<n8-row;col++)
+        //     {
+        //         System.out.print("*");
+        //     }
+
+        //     System.out.println();
+        // }
+
+
+        // // second part 
+        // for(int row =0; row<n8; row++)
+        // {
+        //     // Ist traingle
+        //     for(int col=0;col<row+1;col++)
+        //     {
+        //         System.out.print("*");
+        //     }
+
+        //     // space wala full inverted pyramid 
+        //     for(int col =0; col<2*n8-2*row - 1; col++)   // 2*n8-(2*row + 1) => 2*n8 - 2*row -1
+        //     {
+        //         System.out.print(" ");
+        //     }
+
+        //     // Last traingle
+        //     for(int col=0;col<row+1;col++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Fancy Pattern #2
+
+        // System.out.println("Enter value of n9 for fancy pattern #2");
+        // int n9 = sc.nextInt();
+
+        // // 1st part
+        // for(int row =0; row<n9; row++)
+        // {
+        //     for(int col = 0; col<row+1;col++)
+        //     {
+        //         System.out.print(row+1);
+        //         if(col != row)
+        //         {
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // // 2nd part
+        // for(int row =0; row<n9; row++)
+        // {
+        //     for(int col = 0; col<n9-row;col++)
+        //     {
+        //         System.out.print(n9-row);
+        //         if(col != n9-row-1)
+        //         {
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // Alphabet Palindrone Pyramid 
+
+        System.out.println("Enetr value of n10");
+        int n10 = sc.nextInt();
+
+        for(int row =0; row<n10; row++)
         {
-            // space
-            for(int col =0;col<n6-(row + 1);col++)
+            for(int col =0; col<row+1;col++)
             {
-                System.out.print(" ");
+                int ans = col + 1;
+                char ch = (char) (ans + 'A' -1);
+                System.out.print(ch);
             }
 
-            // star 
-            for(int col =0;col<row+1;col++)
+            // reverse counting 
+            for(int col =row; col>=1;col--)
             {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        // Inverted Full Pyramid 
-        for(int row = 0;row<n6;row++)
-        {
-            // space
-            for(int col =0;col<row;col++)
-            {
-                System.out.print(" ");
-            }
-
-            // star 
-            for(int col =0;col<n6-row;col++)
-            {
-                System.out.print("* ");
+                int ans = col;
+                char ch = (char) (ans + 'A' -1);
+                System.out.print(ch);
             }
             System.out.println();
         }
