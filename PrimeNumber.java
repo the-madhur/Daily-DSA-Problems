19f8f65378314ac1d297860a3c95d75329aa0449
the-madhur <madhurhp245@gmail.com>
2025-01-23 13:02:40 +0530

    final commit

---
+++ PrimeNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PrimeNumber 
+{
+    public static String GetPrime(int x)
+    {
+        if(x<=1)
+        {
+            return "Not Prime";
+        }
+        for(int i =2; i<=x/2;i++)   // iterate from 2 to x/2 because any other divisor except from 1 and x found between this iteration
+        {
+            if(x%i==0)  // If x % i == 0, return "Not Prime" because we found a divisor other than 1 and x.
+            {
+                return "Not Prime";
+            }
+        } 
+        return "Prime";
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        {
+            System.out.println("Enter number: ");
+            int n = sc.nextInt();
+
+            String prime =GetPrime(n);
+            System.out.println(prime);
+        }
+        
+    }
+    
+}
