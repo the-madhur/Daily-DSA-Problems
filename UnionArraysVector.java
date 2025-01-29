6198fd989cfb44d6baa80ea69e1a37091d477a1a
the-madhur <madhurhp245@gmail.com>
2025-01-29 10:19:16 +0530

    final commit

--- UnionArraysVector.java
+++ UnionArraysVector.java
@@ -11,17 +11,20 @@ public class UnionArraysVector
 
         Vector<Integer> ans = new Vector();
 
+        // Adding the elements of the first array arr to the vector array ans
         for(int i =0;i<sizea;i++)
         {
             ans.add(arr[i]);
         }
         
+        // Adding the elements of the second array brr to the vector array ans
         for(int i =0;i<sizeb;i++)
         {
             ans.add(brr[i]);
         }
 
         System.out.println("The union of the arrays is: ");
+        //printing the elements of the vector array ans
         for(int i =0;i<ans.size();i++)
         {
             System.out.print(ans.get(i)+" ");
