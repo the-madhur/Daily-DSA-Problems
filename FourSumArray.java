720c791e566697abb9c609238d5fa759240f70f0
the-madhur <madhurhp245@gmail.com>
2025-01-31 15:58:14 +0530

    final commit

--- FourSumArray.java
+++ FourSumArray.java
@@ -3,38 +3,38 @@
 import java.util.Vector;
 public class FourSumArray 
 {
-public static void main(String[] args) 
-{
-    int sum = 100;
-    Vector<Integer> arr = new Vector<>();
-    arr.add(10);
-    arr.add(20);
-    arr.add(30);
-    arr.add(40);
-    
-    for(int i =0;i<arr.size();i++)
+    public static void main(String[] args) 
     {
-        int element1 = arr.get(i);
-        for(int j =i+1; j<arr.size();j++)
+        int sum = 100;
+        Vector<Integer> arr = new Vector<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        
+        for(int i =0;i<arr.size();i++)
         {
-            int element2 = arr.get(j);
-            
-            for(int k =j+1;k<arr.size();k++)
+            int element1 = arr.get(i);
+            for(int j =i+1; j<arr.size();j++)
             {
-                int element3 = arr.get(k);
-                for(int l = k+1;l<arr.size();l++)
-                {
-                    int element4 = arr.get(l);
-                    if(element1+element2+element3+element4==sum)
-                    {
-                        System.out.println("{"+element1+","+element2+","+element3+","+element4+"}");
-                    }
-                }
+                int element2 = arr.get(j);
                 
+                for(int k =j+1;k<arr.size();k++)
+                {
+                    int element3 = arr.get(k);
+                    for(int l = k+1;l<arr.size();l++)
+                    {
+                        int element4 = arr.get(l);
+                        if(element1+element2+element3+element4==sum)
+                        {
+                            System.out.println("{"+element1+","+element2+","+element3+","+element4+"}");
+                        }
+                    }
+                    
+                }
             }
         }
     }
-}
     
 }
 
---
+++ SortZeroAndOne.java
@@ -0,0 +1,43 @@
+import java.util.Vector;
+public class SortZeroAndOne 
+{
+    public static void swap(Vector<Integer> arr, int i, int j) 
+    {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+
+    public static void main(String[] args) {
+        Vector<Integer> arr = new Vector<>();
+        arr.add(0);
+        arr.add(1);
+        arr.add(1);
+        arr.add(0);
+        arr.add(1);
+        arr.add(0);
+        arr.add(1);
+        arr.add(0);
+        arr.add(0);
+
+        int start = 0;
+        int end = arr.size() - 1;
+
+        while (start < end) {
+            while (start < end && arr.get(start) == 0) 
+            {
+                start++;
+            }
+            while (start < end && arr.get(end) == 1) {
+                end--;
+            }
+            if (start < end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+
+        System.out.println("After sorting 0 and 1: " + arr);
+    }
+}
