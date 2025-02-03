7eae9833fda2ca0e9bf653b51f20e1f8227ffe34
the-madhur <madhurhp245@gmail.com>
2025-02-03 12:12:31 +0530

    final commit

---
+++ ColumnWiseSum.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ColumnWiseSum 
+{
+    public static void printColumnWise(int arr[][], int rows, int cols)
+    {
+        for(int i =0;i<rows;i++)
+        {
+            int sum =0;
+            for(int j =0;j<cols;j++)
+            {
+                sum = sum + arr[j][i];   // here changes made only
+            }
+            System.out.print(sum);
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = new int [3][3];
+        int rows = 3;
+        int cols = 3;
+    
+        for(int i =0;i<rows;i++)
+        {
+            for(int j =0;j<cols;j++)
+            {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i =0;i<rows;i++)
+        {
+            for(int j =0;j<cols;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        printColumnWise(arr, rows, cols);
+        
+    }   
+    
+}
